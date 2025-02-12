2222ff9f489c63d5fca2be34fa601a870f9179c2
sahil khan <khanshil504@gmail.com>
2025-02-12 18:07:52 +0530

    ..

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -62,7 +62,7 @@ module.exports.loginUser = async function (req, res) {
         res.cookie("token", token, {
           httpOnly: true,
           secure: true,
-          sameSite: "None",
+          sameSite: "lax",
           domain:''
         });
         
