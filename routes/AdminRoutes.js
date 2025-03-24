b2d3e58d63bc3e0ccba9bfefe297bbfcee407628
sahil khan <khanshil504@gmail.com>
2025-03-24 15:07:24 +0530

    slight change admin route

--- routes/AdminRoutes.js
+++ routes/AdminRoutes.js
@@ -92,9 +92,11 @@ router.get("/totalAppointment", async  (req,res)=>{
         patientemail: 1,
         appointmentDate: 1,
         appointmentStatus: 1,
+        mainUser : " $userDetails.userName",
         doctorName: "$doctorDetails.name", // ✅ Fetching doctor's name
         doctorEmail: "$doctorDetails.email", // ✅ Fetching doctor's email
         department: "$departmentDetails.name", // ✅ Fetching department name
+         
       },
     },
   ])
