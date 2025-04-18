a4256f27b8ffc26dd0b8e8f9e9903742d2a57561
sahil khan <khanshil504@gmail.com>
2025-04-18 14:58:03 +0530

    commiting two files for isDocLoggedin.js testing on live

--- routes/DoctorDashboard.js
+++ routes/DoctorDashboard.js
@@ -3,29 +3,15 @@ const mongoose = require('mongoose');
 const router = express.Router();
 // const Doctor = require("../models/DoctorSchema");
 const Appointment = require("../models/Appointment");
-const doctor = require("../models/DoctorSchema");
-const isLoggedIn = require("../middlewares/IsLoggedin");
 // const Department = require("../models/DepartmentSchema");
+const isdoctorLoggedin = require("../middlewares/isDocLoggedin");
 
-router.get("/allAppointments/", isLoggedIn, async (req, res) => {
+router.get("/allAppointments/",isdoctorLoggedin, async (req, res) => {
     try {
-      // const id = '67cd58ac44384dfc0993323b';
-      // const appointments = await Appointment.find(
-      //   {doctorId: new mongoose.Types.ObjectId(id) ,
-      //      isDeleted: false }, // only fetch non-deleted appointments
-      //   {
-      //     appointmentStatus: 0,
-      //     availability: 0,
-      //     userId: 0,
-      //     departmentId: 0,
-      //     isDeleted:0,
-      //     deletedAt:0,
-      //     deletedBy:0,
-      //     // appointmentDate:1
-
-      const appointments = await doctor.find({});
-        
-      res.status(200).json(appointments);
+    console.log("This is doctor id", req.doctorId);
+    
+    
+      res.status(200).json({hello});
     } catch (err) {
       console.log("This is catch error", err.message);
       res.status(500).json({ error: err.message });
